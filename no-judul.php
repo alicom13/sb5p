91f2d9443b4f561129587159a0ef4977722b3a1a
Ali Musthofa <alfaonline13@gmail.com>
2023-07-30 16:04:46 +0700

    Create no-judul.php

---
+++ no-judul.php
@@ -0,0 +1,11 @@
+<?php get_header();
+/* Template Name: No Judul */
+?>
+    <main>
+        <section>
+            <div class="container pt-5 pb-5 d-flex justify-content-center">
+              <?php the_content(); ?>
+            </div>
+        </section>
+    </main>
+<?php get_footer(); ?>
